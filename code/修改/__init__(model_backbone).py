563e1d12d96d0c259d02b9c51182674a7cc854cb
ZZQ987 <166967388+ZZQ987@users.noreply.github.com>
2024-07-09 12:52:25 +0800

    Add files via upload

---
+++ code/修改/__init__(model_backbone).py
@@ -0,0 +1,23 @@
+from .resnet import *
+from .vit import vit_pt_imnet,vit_inflora,vit_inflora_b5
+
+
+# from .sinet import *
+def get_backbone(config):
+    """
+    Get the backbone according to the config dict.
+
+    Args:
+        config: The config dict.
+
+    Returns: The backbone module.
+    """
+
+    kwargs = dict()
+    kwargs.update(config['backbone']['kwargs'])
+    try:
+        emb_func = eval(config["backbone"['name']])(**kwargs)
+    except NameError:
+        raise ("{} is not implemented".format(config["backbone"]['name']))
+    
+    return emb_func
